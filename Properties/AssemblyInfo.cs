9f15b6d57d83133b6a8f85db22d113ca8f1c7855
yeetmydog <48489278+yeetmydog@users.noreply.github.com>
2020-03-09 19:20:14 +0100

    Add source (2)

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Acenoid 2.5")]
+[assembly: AssemblyDescription("Powerful open-source ROBLOX exploit with triple API functionality.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("awsomequickness")]
+[assembly: AssemblyProduct("Acenoid 2.5")]
+[assembly: AssemblyCopyright("Copyright © awsomequickness 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("598ccbf0-00b2-4a34-8062-02412d3cb9da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
